197926b3fcc9c6b89523b0512f88bee4dcb3e37e
Viktoria <zaharova181114@gmail.com>
2022-01-23 00:14:22 +0200

    Initial commit

---
+++ app/js/common.js
@@ -0,0 +1,6 @@
+$('.history-clients-slider').slick({
+    slidesToShow: 1,
+    fade: true,
+    prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
+    nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
+});
