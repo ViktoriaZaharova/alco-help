8d7ce2f31868e3f4cbc6e069e64d99d729ecc1f8
Viktoria <zaharova181114@gmail.com>
2022-01-23 10:12:13 +0200

    add blocks

--- app/js/common.js
+++ app/js/common.js
@@ -4,3 +4,24 @@ $('.history-clients-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-left"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></button>'
 });
+
+$('.submenu-item__links').on('click', function (e) {
+    e.preventDefault();
+    $(this).toggleClass('open').siblings('.dropdown-menu__sub').slideToggle();
+});
+
+
+$('.dropdown-services .dropdown-menu').on('click', function(event){
+    // The event won't be propagated up to the document NODE and
+    // therefore delegated events won't be fired
+    event.stopPropagation();
+});
+
+$('.btn-burger').on('click', function () {
+   $('.mobile-menu').fadeToggle();
+});
+
+$('.btn-close').on('click', function (e) {
+    e.preventDefault();
+    $('.mobile-menu').fadeOut();
+});
